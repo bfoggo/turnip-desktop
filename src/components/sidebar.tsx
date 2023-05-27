bfc849f7e28b1339e06aa85a61b4fda3c88404aa
bfoggo <brandon.foggo@habitat.energy>
2023-05-27 16:38:25 -0500

    sidebar alignment

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -11,7 +11,7 @@ export const Sidebar = (props: SidebarProps) => {
     return (
         <aside className='fixed top-0 left-0 w-32 h-screen sidebar'>
             <div className='h-full flex flex-col items-center'>
-                <ul className='pt-10 items-center space-y-2 justify-center'>
+                <ul className='pt-9 items-center space-y-2 justify-center'>
                     <li className="sidebar-text">
                         <CampaignLinkedTitle campaign_name={props.campaign_name} campaign_id={props.campaign_id} title='Dashboard' icon="IdentificationIcon" />
                     </li>
@@ -39,7 +39,7 @@ const CampaignLinkedTitle = (props: { campaign_name: string, campaign_id: number
                 }
             }}>
                 <div className="flex flex-row items-center space-x-2">
-                    <p>
+                    <p className="pt-1">
                         {props.title}
                     </p>
                     {/* @ts-ignore */}
